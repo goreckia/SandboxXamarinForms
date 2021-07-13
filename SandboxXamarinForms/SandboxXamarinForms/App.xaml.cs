df78323903ed00a35ec31cfcf5bf540d8398e454
Arthur Gorecki <gruhtra@mac.com>
2021-07-13 14:50:11 +1000

    Add project files.

---
+++ SandboxXamarinForms/SandboxXamarinForms/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SandboxXamarinForms.Services;
+using SandboxXamarinForms.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SandboxXamarinForms
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SandboxXamarinForms/SandboxXamarinForms/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SandboxXamarinForms.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SandboxXamarinForms.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
